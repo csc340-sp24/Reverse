8982359f0864510ee5a91693d80b28a43a49e0ec
csc340-sp24 <154465695+csc340-sp24@users.noreply.github.com>
2024-01-10 14:44:54 -0500

    Create JavaApplicationX18.java

---
+++ JavaApplicationX18.java
@@ -0,0 +1,13 @@
+public class JavaApplication18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Added a line");
+        System.out.println("Added another line");
+        System.out.println("Line from dev branch");
+    }
+    
+}
